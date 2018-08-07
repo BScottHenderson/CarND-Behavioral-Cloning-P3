11d9772800d734ab6bc223abfe097a4a5bc8b4a2
Scott Henderson <henders999@gmail.com>
2018-08-07 11:28:03 -0700

    Add image cropping to speed training time and reduce unneeded data.

--- model.py
+++ model.py
@@ -33,6 +33,14 @@ IMAGE_HEIGHT   = 160
 IMAGE_WIDTH    = 320
 IMAGE_CHANNELS = 3
 
+# Cropping
+CROP_TOP    = 70
+CROP_BOTTOM = 25
+
+# Cropped image shape:
+CROPPED_IMAGE_HEIGHT = IMAGE_HEIGHT - CROP_TOP - CROP_BOTTOM
+CROPPED_IMAGE_WIDTH  = IMAGE_WIDTH
+
 # Could use trigonometry to find exact adjustment factor if we had enough
 # data regarding the relative position and angle of the three cameras.
 STEERING_CORRECTION = 0.2
@@ -53,6 +61,22 @@ def read_image(source_path):
     image_path = './data/IMG/' + file_name
     image = cv2.imread(image_path)
     return image
+
+
+def read_and_crop_image(source_path):
+    image = read_image(source_path)
+    # Crop the top and bottom of each image. The top of each image
+    # typically shows sky, trees, hills and other data not relevant
+    # to steering while the bottom of each image shows the hood of
+    # the car which is also not relevant to steering.
+    # The purpose of this step is to reduce the amount of data used
+    # for training to improve training speed.
+    # Use numpy slicing to crop the image.
+    # If (x1, y1) is the top/left corner and (x2, y2) is the
+    # bottom/right corner, then:
+    #   cropped_image = image[y1:y2, x1:x2]
+    cropped_image = image[CROP_TOP:IMAGE_HEIGHT - CROP_BOTTOM, 0:IMAGE_WIDTH]
+    return cropped_image
 
 
 def LeNet(model):
@@ -121,9 +145,9 @@ def main(name):
     iterlines = iter(lines)
     next(iterlines)  # skip the first line
     for line in iterlines:
-        images.append(read_image(line[0]))  # center image
-        images.append(read_image(line[1]))  # left image
-        images.append(read_image(line[2]))  # right image
+        images.append(read_and_crop_image(line[0]))  # center image
+        images.append(read_and_crop_image(line[1]))  # left image
+        images.append(read_and_crop_image(line[2]))  # right image
 
         steering = float(line[3])
         measurements.append(steering)  # center image
@@ -134,16 +158,18 @@ def main(name):
     X_train = np.array(images)
     y_train = np.array(measurements)
 
-    image_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+#    image_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+    cropped_image_shape = (CROPPED_IMAGE_HEIGHT, CROPPED_IMAGE_WIDTH, IMAGE_CHANNELS)
 
     model = Sequential()
     # Preprocess incoming data, centered around zero with small standard deviation
     # data preprocessing: normalization and mean centering (shift mean to 0.0)
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=image_shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=cropped_image_shape))
 
     # LeNet
 #    model = LeNet(model)
-    model = NVIDIA(model)
+#    model = NVIDIA(model)
+    model = NVIDIA2(model)
 
     # Use 'mse' (mean squared error) rather than 'cross_entropy' because this is
     # a regression network rather than a classification network.
