2cf0570c7c2e4b4545035b3fe62613d5acf8ce44
Scott Henderson <henders999@gmail.com>
2018-08-07 11:21:37 -0700

    Initial implementation with LeNet.

---
+++ model.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 11:11:50 2018
+
+@author: henders
+
+Self-Driving Car Engineer Nanodegree
+
+Project: Behavioral Cloning
+"""
+
+import sys
+import csv
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda  # , Cropping2D
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dropout
+from keras import regularizers
+
+from image_augmentation import add_brightness, add_shadow, add_snow, add_rain, add_fog
+
+
+#
+# Hyperparameters
+#
+
+# Input image shape:
+IMAGE_HEIGHT   = 160
+IMAGE_WIDTH    = 320
+IMAGE_CHANNELS = 3
+
+# Training parameters
+VALIDATION_SPLIT = 0.2
+EPOCHS           = 5
+
+
+#
+# Local functions
+#
+
+def read_image(source_path):
+    file_name = source_path.split('/')[-1]
+    image_path = './data/IMG/' + file_name
+    image = cv2.imread(image_path)
+    return image
+
+
+def LeNet(model):
+    model.add(Convolution2D(6, (5, 5), activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, (5, 5), activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+
+#
+# Main
+#
+
+def main(name):
+
+    print('Name: {}'.format(name))
+
+    print('Read driving log ...')
+    lines = []
+    with open('./data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+
+    print('Load image files ...')
+    images = []
+    measurements = []
+    iterlines = iter(lines)
+    next(iterlines)  # skip the first line
+    for line in iterlines:
+        images.append(read_image(line[0]))  # center image
+
+        steering = float(line[3])
+        measurements.append(steering)  # center image
+
+    print('Train the model ...')
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+
+    image_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+    model = Sequential()
+    # Preprocess incoming data, centered around zero with small standard deviation
+    # data preprocessing: normalization and mean centering (shift mean to 0.0)
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=image_shape))
+
+    # LeNet
+    model = LeNet(model)
+
+    # Use 'mse' (mean squared error) rather than 'cross_entropy' because this is
+    # a regression network rather than a classification network.
+    model.compile(loss='mae', optimizer='adam')
+    model.fit(X_train, y_train,
+              validation_split=VALIDATION_SPLIT, shuffle=True,
+              epochs=EPOCHS)
+
+#    model.save('model.h5')
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
